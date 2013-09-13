5bd2998a2759702cc1890be2f7905077862186b5
Vishal Anand <exvetsailor@gmail.com>
2013-09-13 15:58:10 -0700

    ''

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,14 @@ Blog::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 =>  587,
+    :user_name            => 'exvetsailor@gmail.com',
+    :password             => 'kaiviti1',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 end
